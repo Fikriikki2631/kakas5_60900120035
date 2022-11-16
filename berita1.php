8cc3b5b8567fbd73b362c231d7a758000d1d08c5
Fikriikki2631 <muhfikrimathar@gmail.com>
2022-11-16 14:04:50 +0700

    berita1.php added

---
+++ berita1.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h2 align=center>Hasil Aston Villa Vs Man United: Ronaldo-Garnacho Starter, Setan Merah Kalah</h2>
+    <p align=center><img src=Gambarmu.png></p>
+    <p align=center>Aston Villa sukses mempermalukan Manchester United dalam duel pekan ke-15 Premier League</p>
+    <p align=center>2022/2023, Minggu (6/11/2022). Bermain di Villa Park, tuan rumah menghajar Setan Merah dengan skor 3-1.</p>
+
+</body>
+</html>
